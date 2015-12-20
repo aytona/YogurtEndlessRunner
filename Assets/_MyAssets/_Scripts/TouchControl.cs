4a45fed86a6754b5f9f6cbf5c1d8ea36c962a484
CarloAlbino <carloalbino@gmail.com>
2015-12-20 14:17:25 -0500

    Touch Controls
    -Started working on touch controls, not fully implemented yet.
    -Switched the project to iOS

---
+++ Assets/_MyAssets/_Scripts/TouchControl.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum SwipeDirection {up, down, left, right};
+
+public class TouchControl : MonoBehaviour {
+    private Vector2 startPos;
+    private Vector2 direction;
+    public bool directionChosen;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DetectSwipe();
+	}
+
+    private void DetectSwipe()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startPos = touch.position;
+                    directionChosen = false;
+                    Debug.Log("Touch");
+                    break;
+                case TouchPhase.Moved:
+                    direction = touch.position - startPos;
+                    break;
+                case TouchPhase.Ended:
+                    directionChosen = true;
+                    break;
+            }
+        }
+        if (directionChosen)
+        {
+            if (direction.y > 0 && direction.y > direction.x)
+            {
+                Debug.Log("Swipe Up");
+            }
+            if (direction.y < 0 && direction.y < direction.x)
+            {
+                Debug.Log("Swipe Down");
+            }
+            if (direction.x > 0 && direction.x > direction.y)
+            {
+                Debug.Log("Swipe Right");
+            }
+            if (direction.x < 0 && direction.x < direction.y)
+            {
+                Debug.Log("Swipe Left");
+            }
+        }
+    }
+}
