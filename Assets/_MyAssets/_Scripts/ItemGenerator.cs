a595d52a5496a5048652562019ca31a8cf283788
aytona <christopher.aytona@gmail.com>
2015-12-20 18:54:24 -0500

    Generic object pooling script added, Item Generator script started

---
+++ Assets/_MyAssets/_Scripts/ItemGenerator.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemGenerator : MonoBehaviour {
+
+    #region Variables
+
+    // List of items/objects used later when needed
+    // [Tooltip("List of items to generate")]
+    // public GameObject[] itemPrefabArray;
+    //[Tooltip("List of obstacles to generate")]
+    //public GameObject[] obstaclePrefabArray;
+
+    [Tooltip("Candy to be spawned")]
+    public GameObject item;
+
+    [Tooltip("Obstacle to be spawned")]
+    public GameObject obstacle;
+
+    [Tooltip("Array of targets, where the items and obstacles generate at")]
+    [SerializeField]
+    private Transform[] targets;
+
+    [Tooltip("Item Ratio")]
+    public int itemRatio;
+
+    [Tooltip("Obstacle Ratio")]
+    public int obstacleRatio;
+
+    [Tooltip("Current level")]
+    [SerializeField]
+    private int level = 0;
+
+    // List of GameObjects to pool
+    List<GameObject> items;
+    List<GameObject> obstacles;
+
+    #endregion Variables
+
+    #region Monobehaviour
+
+    void Start()
+    {
+        // Pool item objects
+        items = new List<GameObject>();
+        for (int i = 0; i < itemRatio; i++)
+        {
+            GameObject obj = Instantiate(item);
+            obj.SetActive(false);
+            items.Add(obj);
+        }
+
+        // Pool obstacle objects
+        obstacles = new List<GameObject>();
+        for (int i = 0; i < obstacleRatio; i++)
+        {
+            GameObject obj = Instantiate(obstacle);
+            obj.SetActive(false);
+            obstacles.Add(obj);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    #endregion Monobehaviour
+}
---
+++ Assets/_MyAssets/_Scripts/ObjectPooling.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPooling : MonoBehaviour {
+
+    private static ObjectPooling current;
+    public GameObject pooledObject;
+
+    private int pooledAmount;
+    private bool willGrow = true;
+    private List<GameObject> pooledObjects;
+
+    void Awake()
+    {
+        current = this;
+    }
+
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        for (int i = 0; i < pooledAmount; i++)
+        {
+            GameObject obj = Instantiate(pooledObject);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+                return pooledObjects[i];
+        }
+
+        if (willGrow)
+        {
+            GameObject obj = Instantiate(pooledObject);
+            pooledObjects.Add(obj);
+            return obj;
+        }
+
+        return null;
+    }
+}
