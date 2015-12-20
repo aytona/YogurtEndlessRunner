2946494e431e6040a6db8bdbe1738488bf4beca3
aytona <christopher.aytona@gmail.com>
2015-12-20 14:56:53 -0500

    Platform movement script added, and separated each models to be their own object

---
+++ Assets/_MyAssets/_Scripts/PlatformMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformMovement : MonoBehaviour {
+
+    #region Variables
+
+    [Tooltip("Speed of the platform")]
+    public float speed;
+
+    [Tooltip("Width of the platform")]
+    public float widthOfPlatform;
+
+    [Tooltip("Position of where the platform starts to despawn")]
+    public float lengthBeforeDespawn;
+
+    #endregion Variables
+
+    #region Monobehaviour
+
+    void FixedUpdate()
+    {
+        // Moves the platform to the left
+        if (transform.position.x >= lengthBeforeDespawn)
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        // Relocate the platform to the front
+        else
+            transform.Translate(widthOfPlatform, 0, 0);
+    }
+
+    #endregion Monobehavious
+}
