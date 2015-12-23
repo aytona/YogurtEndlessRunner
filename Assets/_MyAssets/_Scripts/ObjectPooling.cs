f8e03eb31bb1e98e2460569006a566310d93536e
aytona <christopher.aytona@gmail.com>
2015-12-23 05:36:00 -0500

    Object and Obstacle spawner completed
    - Note: May want to add a rigidbody to the Object and Obstacles so they
    can move along the platform, rather than setting a speed on each object
    to move at and trying to match it with the speed of the platform.

--- Assets/_MyAssets/_Scripts/ObjectPooling.cs
+++ Assets/_MyAssets/_Scripts/ObjectPooling.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 
+// Note: For future use, and reference
 public class ObjectPooling : MonoBehaviour {
 
-    private static ObjectPooling current;
-    public GameObject pooledObject;
+    public static ObjectPooling current;
 
+    #region Variables
+
+    public GameObject pooledObject;
     private int pooledAmount;
     private bool willGrow = true;
     private List<GameObject> pooledObjects;
+
+    #endregion Variables
+
+    #region Monobehaviour
 
     void Awake()
     {
@@ -26,6 +33,10 @@ public class ObjectPooling : MonoBehaviour {
             pooledObjects.Add(obj);
         }
     }
+
+    #endregion Monobehaviour
+
+    #region Public Methods
 
     public GameObject GetPooledObject()
     {
@@ -44,4 +55,6 @@ public class ObjectPooling : MonoBehaviour {
 
         return null;
     }
+
+    #endregion Public Methods
 }
