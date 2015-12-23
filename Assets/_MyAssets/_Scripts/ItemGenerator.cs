6ecac7e81e37d67f1c9b473702a0a8dc4c4c17e3
aytona <christopher.aytona@gmail.com>
2015-12-23 13:39:29 -0500

    Re-Upload game files
    - Carlo's version

--- Assets/_MyAssets/_Scripts/ItemGenerator.cs
+++ Assets/_MyAssets/_Scripts/ItemGenerator.cs
@@ -2,15 +2,11 @@
 using System.Collections;
 using System.Collections.Generic;
 
+// Note: This script can only generate 1 variation of each type of objects
+// Option: Just change the mesh/obj looks through a different script
 public class ItemGenerator : MonoBehaviour {
 
     #region Variables
-
-    // List of items/objects used later when needed
-    // [Tooltip("List of items to generate")]
-    // public GameObject[] itemPrefabArray;
-    //[Tooltip("List of obstacles to generate")]
-    //public GameObject[] obstaclePrefabArray;
 
     [Tooltip("Candy to be spawned")]
     public GameObject item;
@@ -28,13 +24,22 @@ public class ItemGenerator : MonoBehaviour {
     [Tooltip("Obstacle Ratio")]
     public int obstacleRatio;
 
+    [Tooltip("Spawn Repeat Rate")]
+    public float repeatRate;
+
+    // Probably should just save level number in player prefs and get it from there
     [Tooltip("Current level")]
     [SerializeField]
     private int level = 0;
 
     // List of GameObjects to pool
-    List<GameObject> items;
-    List<GameObject> obstacles;
+    private List<GameObject> items;
+    private List<GameObject> obstacles;
+
+    // List of everything to spawn
+    private List<GameObject> spawns;
+
+    private bool willGrow = true;
 
     #endregion Variables
 
@@ -42,6 +47,9 @@ public class ItemGenerator : MonoBehaviour {
 
     void Start()
     {
+        // TODO: Current level number should affect the itemRatio and obstacleRatio in someway.
+        // EX: itemRatio *= level * 0.x; (Or any variation)
+
         // Pool item objects
         items = new List<GameObject>();
         for (int i = 0; i < itemRatio; i++)
@@ -59,12 +67,45 @@ public class ItemGenerator : MonoBehaviour {
             obj.SetActive(false);
             obstacles.Add(obj);
         }
-    }
 
-    void Update()
-    {
-
+        // Initiate Spawns - Maybe better to make it into a Queue
+        spawns = new List<GameObject>();
+        foreach (GameObject i in items)
+            spawns.Add(i);
+        foreach (GameObject i in obstacles)
+            spawns.Add(i);
+        for (int i = 0; i < spawns.Count; i++)
+        {
+            GameObject temp = spawns[i];
+            int randIndex = Random.Range(i, spawns.Count);
+            spawns[i] = spawns[randIndex];
+            spawns[randIndex] = temp;
+        }
+        
+        InvokeRepeating("Spawn", 1f, repeatRate);
     }
 
     #endregion Monobehaviour
+
+    #region Private Methods
+    
+    private void Spawn()
+    {
+        for (int i = 0; i < spawns.Count; i++)
+        {
+            int randTarget = Random.Range(0, targets.Length);
+
+            // Note: Whenever an object deactivates, it will reactive the same one
+            // Current work around is making the number of objects and obstacles small
+            if (!spawns[i].activeInHierarchy)
+            {
+                spawns[i].transform.position = targets[randTarget].transform.position;
+                spawns[i].transform.rotation = targets[randTarget].transform.rotation;
+                spawns[i].SetActive(true);
+                break;
+            }
+        }
+    }
+
+    #endregion Private Methods
 }
