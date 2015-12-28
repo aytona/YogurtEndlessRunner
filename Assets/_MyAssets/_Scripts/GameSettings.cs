85b568bdba13cf920c66d8d7b45a797c76dcf726
aytona <christopher.aytona@gmail.com>
2015-12-27 23:42:43 -0500

    Singleton.cs added

---
+++ Assets/_MyAssets/_Scripts/GameSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class GameSettings : MonoBehaviour {
+
+    [Tooltip("The speed of the objects")]
+    public float gameSpeed;
+
+    [Tooltip("The weight of the player")]
+    public float playerWeight;
+
+}
