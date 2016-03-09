c048b73e9e7e84216f9f93ef46d2a6f5d7745a36
CarloAlbino <carloalbino@gmail.com>
2016-03-08 19:14:28 -0500

    Device and aspect ratio detection
    Modified GameManager to detect the iOS device, if running on an iOS
    device, or detect the screen's aspect ratio when not on iOS.
    
    Tested at the start screen.  The start screen changes depending on the
    aspect ratio.

--- Assets/_MyAssets/_Scripts/GameManager.cs
+++ Assets/_MyAssets/_Scripts/GameManager.cs
@@ -1,6 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum DeviceAspect{
+    iPhone4,        // 3:2
+    iPhone5,        // 16:9
+    iPad,           // 4:3
+    Unknown, 
+    DeviceAspectCount
+}
+
 // Everything that needs to be a single should be put in here
 public class GameManager : Singleton<GameManager> {
 
@@ -9,4 +17,71 @@ public class GameManager : Singleton<GameManager> {
 
     [Tooltip("The x-position of where objects get despawned")]
     public float lengthBeforeDespawn;
+
+    /// <summary>
+    /// The aspect ratio of device the game is currently being played on.  Only updated on Awake.
+    /// </summary>
+    public DeviceAspect currentAspect { get { return aspect; } }
+    private DeviceAspect aspect;
+
+    // Used in editor
+    private float iPhone4Aspect = 1.5f;
+    private float iPhone5Aspect = 1.77f;
+    private float iPadAspect = 1.33f;
+
+#if UNITY_EDITOR
+
+    void Awake()
+    {
+        if (Camera.main.aspect >= iPhone5Aspect)
+        {
+            // 16:9
+            aspect = DeviceAspect.iPhone5;
+        }
+        else if (Camera.main.aspect >= iPhone4Aspect)
+        {
+            // 3:2
+            aspect = DeviceAspect.iPhone4;
+        }
+        else if (Camera.main.aspect >= iPadAspect)
+        {
+            // 4:3
+            aspect = DeviceAspect.iPad;
+        }
+        else
+        {
+            // Unknown
+            Debug.Log("Device aspect ratio is unknown.  Aspect ratio is: " + Camera.main.aspect);
+            aspect = DeviceAspect.Unknown;
+        }
+    }
+
+#elif UNITY_IOS
+
+    void Awake()
+    {
+        if (SystemInfo.deviceModel.Contains("Unknown"))
+        {
+            // Unknown iOS device
+            Debug.Log("Device is unknown.  Aspect ratio is: " + Camera.main.aspect);
+            aspect = DeviceAspect.Unknown;
+        }
+        else if (SystemInfo.deviceModel.Contains("iPad"))
+        {
+            // iPad or iPad mini device
+            aspect = DeviceAspect.iPad;
+        }
+        else if (SystemInfo.deviceModel.Contains("5") || SystemInfo.deviceModel.Contains("6"))
+        {
+            // iPhone 5 or iPhone 6 device
+            aspect = DeviceAspect.iPhone5;
+        }
+        else
+        {
+            // iPhone 4 or iPod Touch or older
+            aspect = DeviceAspect.iPhone4;
+        }
+    }
+
+#endif
 }
