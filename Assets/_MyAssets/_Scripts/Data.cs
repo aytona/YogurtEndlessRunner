ebc94aab55f5fa682197ae96b34751ffc0f1ddce
aytona <christopher.aytona@gmail.com>
2016-03-16 23:27:20 -0400

    Added getter and setter for all totals, need to implement

--- Assets/_MyAssets/_Scripts/Data.cs
+++ Assets/_MyAssets/_Scripts/Data.cs
@@ -1,7 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// This is the script that will handle all savings (Playerprefs)
 public class Data : Singleton<Data> 
 {
-	
+    private Data() { }
+
+
+    public void SetTotalDistance(float distance)
+    {
+        PlayerPrefs.SetFloat("TotalDistance", distance);
+    }
+
+    public float GetTotalDistance()
+    {
+        float totalDistance = PlayerPrefs.GetFloat("TotalDistance", 0);
+        return totalDistance;
+    }
+
+    public void SetTotalScore(int playerScore)
+    {
+        PlayerPrefs.SetInt("TotalScore", playerScore);
+    }
+
+    public int GetTotalScore()
+    {
+        int totalScore = PlayerPrefs.GetInt("TotalScore", 0);
+        return totalScore;
+    }
 }
