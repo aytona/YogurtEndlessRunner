76243777d1db0111874d1fe332631f389f747f9e
aytona <christopher.aytona@gmail.com>
2016-05-09 06:50:00 -0400

    Flag spawner thing added

--- Assets/_MyAssets/_Scripts/MarkerSpawner.cs
+++ Assets/_MyAssets/_Scripts/MarkerSpawner.cs
@@ -13,7 +13,7 @@ public class MarkerSpawner : MonoBehaviour {
 	[Tooltip("The prefab of the flag")]
 	public GameObject _flagPrefab;
 
-	[Tooltip("The distance the flag spawns on every..")]
+	[Tooltip("The distance the flag spawns on every..\tNeeds to be min 32m")]
 	public int repeatDistance;
 
 	[Tooltip("Player object")]
@@ -25,41 +25,42 @@ public class MarkerSpawner : MonoBehaviour {
 	private int counter = 1;
 
 	private float offset;
+	private float nextDistance;
 
 	private bool setOffset = false;
-	private bool flagSpawn = false;
+	private bool flagSpawned = false;
 
-	void LateUpdate() {
-//		if ((GameManager.Instance.gameSettings.distance /* - offset */) % repeatDistance == 0 &&
-//			GameManager.Instance.gameSettings.gameStart) {
-//
-//		}
+	void Start() {
+		nextDistance = repeatDistance * counter;
+		if (player == null) {
+			player = FindObjectOfType<PlayerMovement>().parentObject.transform;
+		}
+	}
 
+	void Update() {
 		if (GameManager.Instance.gameSettings.gameStart && !setOffset) {
 			offset = transform.position.x - player.transform.position.x;
+
 			setOffset = true;
+			flagSpawned = false;
 		}
 
 		if (GameManager.Instance.gameSettings.gameRestart && setOffset) {
 			setOffset = false;
 		}
 
-		if (offset > 0 && (int)((GameManager.Instance.gameSettings.distance + offset) * counter) % (int)(repeatDistance * counter) == 0
-			&& !flagSpawn) {
-			flagSpawn = true;
-		}
-
-		else if (flagSpawn) {
-			flagSpawn = false;
+		else if (offset > 0 && !flagSpawned &&
+			(int)(GameManager.Instance.gameSettings.distance + offset) % (int)(nextDistance) == 0) {
 			SpawnFlag();
 		}
-
-		Debug.Log((int)GameManager.Instance.gameSettings.distance + offset);
 	}
 
 	private void SpawnFlag() {
-		GameObject flag = Instantiate(_flagPrefab, transform.position, Quaternion.identity) as GameObject;
-		flag.GetComponent<DistanceMarker>().distanceText = (repeatDistance * counter).ToString();
+		flagSpawned = true;
+		nextDistance += repeatDistance;
 		counter++;
+		GameObject flag = Instantiate(_flagPrefab, transform.position, Quaternion.identity) as GameObject;
+		flag.GetComponent<DistanceMarker>().distanceText = (repeatDistance * (counter - 1)).ToString();
+		flagSpawned = false;
 	}
 }
