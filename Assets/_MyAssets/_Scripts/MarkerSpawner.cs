b16a22205d960da5a624522bec8c632528023d21
CarloAlbino <carloalbino@gmail.com>
2016-05-16 19:42:17 -0400

    Distance Marker now working at each 25m

--- Assets/_MyAssets/_Scripts/MarkerSpawner.cs
+++ Assets/_MyAssets/_Scripts/MarkerSpawner.cs
@@ -30,29 +30,56 @@ public class MarkerSpawner : MonoBehaviour {
 	private bool setOffset = false;
 	private bool flagSpawned = false;
 
+    private float iPadOffset = 4.125f;
+    private float iPhone4Offset = 4.305f;
+    private float iPhone5Offset = 4.535f;
+
 	void Start() {
-		nextDistance = repeatDistance * counter;
+        counter = 1;
+        repeatDistance = 25;
+		nextDistance = (repeatDistance - offset) * counter;
 		if (player == null) {
 			player = FindObjectOfType<PlayerMovement>().parentObject.transform;
 		}
+        switch(GameManager.Instance.currentAspect)
+        {
+            case DeviceAspect.iPad:
+                offset = iPadOffset;
+                break;
+            case DeviceAspect.iPhone4:
+                offset = iPhone4Offset;
+                break;
+            case DeviceAspect.iPhone5:
+                offset = iPhone5Offset;
+                break;
+        }
 	}
 
 	void Update() {
-		if (GameManager.Instance.gameSettings.gameStart && !setOffset) {
+		/*if (GameManager.Instance.gameSettings.gameStart && !setOffset) {
 			offset = transform.position.x - player.transform.position.x;
 
 			setOffset = true;
 			flagSpawned = false;
-		}
+		}*/
 
-		if (GameManager.Instance.gameSettings.gameRestart && setOffset) {
+        if(!setOffset)
+        {
+            setOffset = true;
+            flagSpawned = false;
+        }
+
+		/*if (GameManager.Instance.gameSettings.gameRestart && setOffset) {
 			setOffset = false;
-		}
+		}*/
 
 		else if (offset > 0 && !flagSpawned &&
 			(int)(GameManager.Instance.gameSettings.distance + offset) % (int)(nextDistance) == 0) {
-                Debug.Log("Spawning Flag");
-			    SpawnFlag();
+                if (nextDistance - GameManager.Instance.gameSettings.distance < 1.0f + offset) // Should prevent multiple spawns at once
+                {
+                    Debug.Log("Spawning Flag");
+                    SpawnFlag();
+                }
 		}
 	}
 
@@ -61,7 +88,10 @@ public class MarkerSpawner : MonoBehaviour {
 		nextDistance += repeatDistance;
 		counter++;
 		GameObject flag = Instantiate(_flagPrefab, transform.position, Quaternion.identity) as GameObject;
-		flag.GetComponent<DistanceMarker>().distanceText = (repeatDistance * (counter - 1)).ToString();
+		flag.GetComponent<DistanceMarker>().distanceText = (repeatDistance * (counter - 1)).ToString() + "M";
 		flagSpawned = false;
+
+        // Debug.Log(GameManager.Instance.gameSettings.distance);
+        // Debug.Log(GameManager.Instance.gameSettings.distance + offset);
 	}
 }
