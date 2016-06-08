86bd446921a28849227d7b10ce9bbe35407fac7d
CarloAlbino <carloalbino@gmail.com>
2016-06-08 16:51:13 -0400

    Added effects on collision and collect

---
+++ Assets/_MyAssets/_Scripts/EffectController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectController : MonoBehaviour {
+
+    [SerializeField]
+    private string collectEffectName;
+    [SerializeField]
+    private string obstacleHitEffectName;
+    private Animator a;
+
+    void Start()
+    {
+        a = GetComponent<Animator>();
+    }
+
+    public void Collected()
+    {
+        a.Play(collectEffectName);
+    }
+
+    public void Hit()
+    {
+        a.Play(obstacleHitEffectName);
+    }
+}
