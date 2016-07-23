ef7ef13cda6ff20bbea185d66f4e248acc6bce95
aytona <christopher.aytona@gmail.com>
2016-07-23 02:23:07 -0400

    A script that will get a random toppings mesh to set whenever the object is enabled/disabled

--- Assets/_MyAssets/_Scripts/RandomMeshGen.cs
+++ Assets/_MyAssets/_Scripts/RandomMeshGen.cs
@@ -1,7 +1,36 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
-[RequireComponent(typeof(MeshFilter),(typeof(MeshRenderer)))]
+[RequireComponent(typeof(MeshFilter),(typeof(MeshRenderer)), (typeof(Shader)))]
 public class RandomMeshGen : MonoBehaviour {
-    public Mesh[] modelMeshes;
 
+    [System.Serializable]
+    public class Toppings
+    {
+        public Mesh toppingMesh;
+        public Shader toppingShader;
+    }
+
+    /// <summary>
+    /// Set the total number of toppings as size of list
+    /// and drag and drop each mesh and corresponding shader
+    /// </summary>
+    public List<Toppings> toppingsList = new List<Toppings>();
+
+    void OnEnable()
+    {
+        GetRandomTopping();
+    }
+
+    void OnDisable()
+    {
+        GetRandomTopping();
+    }
+
+    private void GetRandomTopping()
+    {
+        Toppings randomTopping = toppingsList[Random.Range(0, toppingsList.Count)];
+        gameObject.GetComponent<MeshFilter>().mesh = randomTopping.toppingMesh;
+        gameObject.GetComponent<Material>().shader = randomTopping.toppingShader;
+    }
 }
