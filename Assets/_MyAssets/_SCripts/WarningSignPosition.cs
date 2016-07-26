f4754093920d35dd128e8aa60662236bb1cf3208
aytona <christopher.aytona@gmail.com>
2016-07-26 00:31:22 -0400

    Added a script that will arrange the warning sign X position

---
+++ Assets/_MyAssets/_SCripts/WarningSignPosition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarningSignPosition : MonoBehaviour {
+
+    public float xOffset;
+
+    void Start()
+    {
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0));
+        Vector3 destination = new Vector3(worldPoint.x - xOffset, 0, 0);
+        gameObject.transform.Translate(destination);
+    }
+}
