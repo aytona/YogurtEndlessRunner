dbaad3f7eda9c6349155d22895d1a9fc5460ddcb
aytona <christopher.aytona@gmail.com>
2016-07-25 23:17:04 -0400

    Large obstacle warning script

---
+++ Assets/_MyAssets/_Scripts/LargeObstacleWarning.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LargeObstacleWarning : MonoBehaviour {
+
+    public GameObject warningSign;
+    public float effectDuration;
+    public float effectDelay;
+
+    void OnEnable()
+    {
+        StartCoroutine(warningEffect(effectDuration, effectDelay));
+    }
+
+    void OnDisable()
+    {
+        StopCoroutine(warningEffect(effectDuration, effectDelay));
+    }
+
+    private IEnumerator warningEffect(float duration, float delay)
+    {
+        bool toggle = true;
+        while (duration > 0)
+        {
+            duration -= Time.deltaTime;
+            warningSign.SetActive(toggle);
+            toggle = !toggle;
+            yield return new WaitForSeconds(delay);
+        }
+        warningSign.SetActive(false);
+    }
+}
