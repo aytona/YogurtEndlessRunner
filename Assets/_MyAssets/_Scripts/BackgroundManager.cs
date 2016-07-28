b26c2333d18e5e2d1ecc637985b374e1a5f3d5f4
aytona <christopher.aytona@gmail.com>
2016-07-27 23:46:51 -0400

    BackgroundManager script added

---
+++ Assets/_MyAssets/_Scripts/BackgroundManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    public GameObject[] ContinousBackground;
+    public GameObject[] ChangingBackground;
+    public float widthOfPlatform;
+    public float distBetweenChange;
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private void Movement()
+    {
+        foreach(GameObject i in ContinousBackground)
+        {
+
+        }
+    }
+}
+
